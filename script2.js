985ac8dba52082e04ee62fd0b6ca16f3b85cecc5
DarkoS <sahid.omir@minutestep.com>
2023-02-01 19:02:34 +0100

    initial commit

--- script2.js
+++
@@ -1,8 +0,0 @@
-var varOne = 0;
-for (i = 0; i < 4; i++) {
-  for (j = 0; j < 4; j++) {
-    varOne++;
-  }
-}
-console.log(varOne);
-/* 16 ispisuje */
--- script3.js
+++
@@ -1,9 +0,0 @@
-var count = 3;
-while (count <= 20) {
-  if (count % 9 === 0) {
-    count++;
-    continue;
-  }
-  console.log(count);
-  count++;
-}
