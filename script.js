91d55943b75c9b41be257bfd83fbfa8425efb808
DarkoS <sahid.omir@minutestep.com>
2023-02-07 21:01:47 +0100

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+/* 1. U folderu /Pocetno se nalaze html i js file kao pocetna tocka.
+2. Uključite script.js u html dokument.
+2. HTML index.html-a treba promijeniti pomocu JS i HTML DOM API-ja tako da:
+	- se na početak doda h1 element
+	- ispod diva sa klasom "description" se umetne novi div sa klasom "info"
+	- lista u footer elementu na kraju mora sadrzavati 5 praznih <li> elemenata */
+
 const descriptionEl = document.querySelector(".description");
 const footerEl = document.querySelector("#footer");
 const ulEl = document.querySelector("ul");
 
 const h1 = document.createElement("h1");
-h1.innerText = "Pametni smo";
+h1.innerText = "Dodan H1";
 
 const infoDiv = document.createElement("div");
 infoDiv.classList.add("info");
