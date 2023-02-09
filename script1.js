a0e4d879eb6a4777500d56fce699992532db530e
DarkoS <sahid.omir@minutestep.com>
2023-02-09 18:22:19 +0100

    initial commit

--- script1.js
+++
@@ -1,9 +0,0 @@
-function sendEmail() {
-  const alertMessageEl = document.querySelector(".alert-message");
-
-  alertMessageEl.style.display = "block";
-
-  setTimeout(() => {
-    alertMessageEl.style.display = "none";
-  }, 3000);
-}
