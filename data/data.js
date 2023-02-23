28333fcc1f9f341170189eab359135853288f970
DarkoS <sahid.omir@minutestep.com>
2023-02-23 21:28:17 +0100

    1st commit

---
+++ data/data.js
@@ -0,0 +1,26 @@
+export const lecture = [
+  {
+    name: "HTML",
+    length: "120min",
+  },
+  {
+    name: "CSS",
+    length: "60min",
+  },
+  {
+    name: "JavaScript",
+    length: "300min",
+  },
+  {
+    name: "GIT",
+    length: "10min",
+  },
+  {
+    name: "React",
+    length: "300min",
+  },
+  {
+    name: "Napredno",
+    length: "90min",
+  },
+];
